a3f61358360d90d8c6f848f0bcf9b09b4358fb45
Malcolm Sioni <sionimalcolm@gmail.com>
2024-12-28 21:26:42 +1000

    update

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const urlsToCache = [
   '/styles.css',
   '/script.js',
   '/manifest.json',
-  'path/to/icon-192x192.png',
-  'path/to/icon-512x512.png'
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
