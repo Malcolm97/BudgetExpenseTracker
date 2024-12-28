43a1966de61f301bc62ca7d267948ca144037b87
Malcolm Sioni <sionimalcolm@gmail.com>
2024-12-28 21:19:35 +1000

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(error) {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     new Vue({
         el: '#app',
